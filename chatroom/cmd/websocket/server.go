3da54539cff2aa8d3fad9cde9f38706161c2d71d
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-04 12:50:08 +0800

    Update server.go

--- chatroom/cmd/websocket/server.go
+++ chatroom/cmd/websocket/server.go
@@ -5,15 +5,16 @@ import (
 	"log"
 	"net/http"
 
-	"nhooyr.io/websocket" // websocket是Go的一个最小且惯用的websocket库
+	"nhooyr.io/websocket" // websocket 是 Go 的一个最小且惯用的 websocket 库
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	// HandleFunc 的功能是绑定路由(第一个参数)和处理器函数(也就是第二个参数)，并注册到 DefaultServeMux 的 map 内
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) { // 对 / 的请求“走” HTTP
 		fmt.Fprintln(w, "HTTP, Hello")
 	})
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) { // 对 /ws 的请求“走” WebSocket
 		conn, err := websocket.Accept(w, req, nil)
 		if err != nil {
 			log.Println(err)
