401f3c49b68b5e78957d731372b2ec039849abfa
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-05 09:44:16 +0800

    Update client.go

--- chatroom/cmd/websocket/client.go
+++ chatroom/cmd/websocket/client.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"context"
+	"nhooyr.io/websocket"
+	"time"
+)
+
 func main() {
-	
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	c, _, err := websocket.Dial(ctx, "ws://localhost:2021/ws", nil)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close(websocket.StatusInternalError, "内部错误！")
 }
