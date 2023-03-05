8621fdcf348f04c5a48846164c35a112fc0fe8e5
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-05 09:59:26 +0800

    Update client.go

--- chatroom/cmd/websocket/client.go
+++ chatroom/cmd/websocket/client.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 	"time"
 )
+
+// http 服务器端默认监听在80端口
+// 因此，抓包只需要监听客户端的指定端口就行
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	c, _, err := websocket.Dial(ctx, "ws://localhost:2021/ws", nil)
+	c, _, err := websocket.Dial(ctx, "ws://localhost:2021/ws", nil) // 这是客户端(也就是浏览器)的端口号
 	if err != nil {
 		panic(err)
 	}
 	defer c.Close(websocket.StatusInternalError, "内部错误！")
+
+	err = wsjson.Write(ctx, c, "Hello WebSocket Server")
+	if err != nil {
+		panic(err)
+	}
+
+	var v interface{} // 空接口，代表所有类型的集合。空接口类型的变量可以存储任何类型的值
+	err = wsjson.Read(ctx, c, &v)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("接收到服务端响应：%v\n", v)
+
+	c.Close(websocket.StatusNormalClosure, "")
 }
