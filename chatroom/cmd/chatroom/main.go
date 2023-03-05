3b56a93a608afce0b75100216cacdd1dd87b4169
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-05 13:34:25 +0800

    Update main.go

--- chatroom/cmd/chatroom/main.go
+++ chatroom/cmd/chatroom/main.go
@@ -1,1 +1,21 @@
-package chatroom
+package main
+
+import "fmt"
+
+var (
+	addr   = ":2022"
+	banner = `
+    ____              _____
+   |    |    |   /\     |
+   |    |____|  /  \    | 
+   |    |    | /----\   |
+   |____|    |/      \  |
+
+	ChatRoom, start on: %s
+`
+)
+
+func main() {
+	fmt.Printf(banner, addr)
+
+}
