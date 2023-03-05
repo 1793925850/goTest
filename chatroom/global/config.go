a01a291b8499afc51ff531b8cc951a3e57684897
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-05 14:05:13 +0800

    添加

---
+++ chatroom/global/config.go
@@ -0,0 +1,11 @@
+package global
+
+var (
+	SensitiveWord []string
+
+	MessageQueueLen = 1024
+)
+
+func initConfig() {
+	
+}
--- chatroom/global/init.go
+++ chatroom/global/init.go
@@ -1,1 +1,24 @@
 package global
+
+import "sync"
+
+// 初始化函数，同包下可见
+func init() {
+	Init()
+}
+
+var RootDir string
+
+var once = new(sync.Once)
+
+// 初始化函数，公共函数
+func Init() {
+	once.Do(func() {
+		inferRootDir()
+		initConfig()
+	})
+}
+
+func inferRootDir() {
+
+}
