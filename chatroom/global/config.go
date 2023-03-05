7e6f854fe36d7d3024fab85fddfcaa5463002c38
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-05 15:19:45 +0800

    Update config.go

--- chatroom/global/config.go
+++ chatroom/global/config.go
@@ -12,19 +12,21 @@ var (
 )
 
 func initConfig() {
-	viper.SetConfigName("chatroom")
-	viper.AddConfigPath(RootDir + "/config")
+	viper.SetConfigName("chatroom")          // SetConfigName 设置配置文件的名称。不包括扩展
+	viper.AddConfigPath(RootDir + "/config") // 添加配置文件的路径，以便在该路径下搜索配置文件
 
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
 
-	SensitiveWords = viper.GetStringSlice("sensitive")
-	MessageQueueLen = viper.GetInt("message-queue")
+	// GetStringSlice 提供字符串切片
+	// GetInt 提供整型数
+	SensitiveWords = viper.GetStringSlice("sensitive") // 提供敏感词
+	MessageQueueLen = viper.GetInt("message-queue")    // 提供消息队列长度
 
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		viper.ReadInConfig()
+	viper.WatchConfig() // WatchConfig 监视配置文件的更改
+	viper.OnConfigChange(func(e fsnotify.Event) { // 当一个配置文件被改动时，OnConfigChange 用来设置这个 event 的 handler
+		viper.ReadInConfig() // 在指定路径下加载配置文件
 
 		SensitiveWords = viper.GetStringSlice("sensitive")
 	})
