7c3e22d36a16d2c95d0b4da57c082e77858a600f
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-08 12:58:47 +0800

    Update broadcast.go

--- chatroom/logic/broadcast.go
+++ chatroom/logic/broadcast.go
@@ -1,1 +1,20 @@
 package logic
+
+import (
+	"expvar"
+	"fmt"
+)
+
+func init() {
+	expvar.Publish("message_queue", expvar.Func(calcMessageQueueLen))
+}
+
+func calcMessageQueueLen() interface{} {
+	fmt.Println("===len=:", len(Broadcaster.messageChannel))
+}
+
+// 广播器
+type broadcaster struct {
+}
+
+var Broadcaster = &broadcaster{}
