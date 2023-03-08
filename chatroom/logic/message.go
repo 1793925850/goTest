2620da16af7dee52440ed2aed2b3321328967c89
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-08 16:32:37 +0800

    Update message.go

--- chatroom/logic/message.go
+++ chatroom/logic/message.go
@@ -1,1 +1,28 @@
 package logic
+
+import "time"
+
+const (
+	MsgTypeNormal    = iota // 普通用户消息
+	MsgTypeWelcome          // 当前用户欢迎消息
+	MsgTypeUserEnter        // 用户进入
+	MsgTypeUserLeave        // 用户退出
+	MsgTypeError            // 错误消息
+)
+
+// 给用户发送的消息
+type Message struct {
+	// 从哪个用户发送的消息
+	User    *User     `json:"user"`
+	Type    int       `json:"type"`
+	Content string    `json:"content"`
+	MsgTime time.Time `json:"msg_time"`
+
+	ClientSendTime time.Time `json:"client_send_time"`
+
+	// 消息 @ 了谁
+	Ats []string `json:"ats"`
+
+	// 用户列表不通过 WebSocket 下发
+	// Users [] *User `json:"users"`
+}
