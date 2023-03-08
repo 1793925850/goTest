9c3305b5004d8a2e801e3c4da2151869b0ec9a8d
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-08 15:47:39 +0800

    Update user.go

--- chatroom/logic/user.go
+++ chatroom/logic/user.go
@@ -1,8 +1,14 @@
 package logic
 
 import (
-	"nhooyr.io/websocket"
+	"context"
+	"errors"
+	"io"
+	"sync/atomic"
 	"time"
+
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 )
 
 // 需要使用 globalUID 来给每个用户创建一个 UID
@@ -26,6 +32,7 @@ type User struct {
 // 系统也是一个用户，即系统用户，代表系统主动发送的消息
 var System = &User{}
 
+// 创建一个用户
 func NewUser(conn *websocket.Conn, token, nickname, addr string) *User {
 	user := &User{
 		NickName:       nickname,
@@ -43,6 +50,53 @@ func NewUser(conn *websocket.Conn, token, nickname, addr string) *User {
 			user.UID = uid
 		}
 	}
+
+	if user.UID == 0 {
+		user.UID = int(atomic.AddUint32(&globalUID, 1))
+		user.Token = genToken(user.UID, user.NickName)
+		user.isNew = true
+	}
+
+	return user
+}
+
+// 用户发送信息
+func (u *User) SendMessage(ctx context.Context) {
+	for msg := range u.MessageChannel {
+		wsjson.Write(ctx, u.conn, msg)
+	}
+}
+
+// CloseMessageChannel 避免 goroutine 泄露
+func (u *User) CloseMessageChannel() {
+	close(u.MessageChannel)
+}
+
+// 用户接收信息
+func (u *User) ReceiveMessage(ctx context.Context) error {
+	var (
+		receiveMsg map[string]string
+		err        error
+	)
+
+	for {
+		err = wsjson.Read(ctx, u.conn, &receiveMsg)
+		if err != nil {
+			// 判定连接是否关闭了，若是正常关闭，则不认为是错误
+			var closeErr websocket.CloseError
+			if errors.As(err, &closeErr) {
+				return nil
+			} else if errors.Is(err, io.EOF) {
+				return nil
+			}
+
+			return err
+		}
+	}
+}
+
+func genToken(uid int, nickname string) string {
+
 }
 
 func parseTokenAndValidate(token string, nickname string) (int, error) {
