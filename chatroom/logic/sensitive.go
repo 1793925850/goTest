2c4610ea4f9d9b81789cce84f3e9cddf05db0d46
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-08 22:31:46 +0800

    Update sensitive.go

--- chatroom/logic/sensitive.go
+++ chatroom/logic/sensitive.go
@@ -1,1 +1,18 @@
 package logic
+
+import (
+	"strings"
+
+	"chatroom/global"
+)
+
+// 过滤敏感词
+func FilterSensitive(content string) string {
+	for _, word := range global.SensitiveWords { // 遍历敏感词表
+		// ReplaceAll 返回将 content 中所有不重叠 word 子串都替换为 new 的新字符串
+		// 即将 content 中的所有 word 替换成 new
+		content = strings.ReplaceAll(content, word, "**") // 如果 content 中有 word，则将 content 的词改为 **
+	}
+
+	return content
+}
