a7c0e686d8643ab32fb59b62dd75e283d0dcfb9c
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-08 22:39:39 +0800

    Update broadcast.go

--- chatroom/logic/broadcast.go
+++ chatroom/logic/broadcast.go
@@ -11,12 +11,28 @@ func init() {
 
 func calcMessageQueueLen() interface{} {
 	fmt.Println("===len=:", len(Broadcaster.messageChannel))
+
+	return len(Broadcaster.messageChannel)
 }
 
 // 广播器
 type broadcaster struct {
 	// 所有聊天室用户
 	users map[string]*User
+
+	// 所有 channel 统一管理，可以避免外部乱用
+
+	enteringChannel chan *User
+	leavingChannel  chan *User
+	messageChannel  chan *Message
+
+	// 判断该昵称用户是否可以进入聊天室（重复与否）：true 能，false 不能
+	checkUserChannel      chan string
+	checkUserCanInChannel chan bool
+
+	// 获取用户列表
+	requestUsersChannel chan struct{}
+	usersChannel        chan []*User
 }
 
 var Broadcaster = &broadcaster{}
