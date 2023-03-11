5e6c3260a898849eaa80c65ad540f91942c0dad2
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-11 12:52:21 +0800

    Update home.go

--- chatroom/server/home.go
+++ chatroom/server/home.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"chatroom/global"
+	"chatroom/logic"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"text/template"
@@ -18,5 +20,19 @@ func homeHandleFunc(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		fmt.Fprint(w, "模板执行错误！")
 		return
+	}
+}
+
+func userListHandleFunc(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	userList := logic.Broadcaster.GetUserList()
+	b, err := json.Marshal(userList)
+
+	if err != nil {
+		fmt.Fprint(w, `[]`)
+	} else {
+		fmt.Fprint(w, string(b))
 	}
 }
