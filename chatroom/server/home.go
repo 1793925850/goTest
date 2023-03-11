5edf76703587124297d2eee137ddaf9a2256bbe7
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-11 11:44:17 +0800

    Update home.go

--- chatroom/server/home.go
+++ chatroom/server/home.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"chatroom/global"
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func homeHandleFunc(w http.ResponseWriter, req *http.Request) {
+	tpl, err := template.ParseFiles(global.RootDir + "/template/home.html")
+	if err != nil {
+		fmt.Fprint(w, "模板解析错误！")
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		fmt.Fprint(w, "模板执行错误！")
+		return
+	}
+}
