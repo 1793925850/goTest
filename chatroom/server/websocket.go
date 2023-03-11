858db7c02217dbe45396d05d3b7361b8fd016e6a
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-11 08:41:55 +0800

    Update websocket.go

--- chatroom/server/websocket.go
+++ chatroom/server/websocket.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"log"
+	"net/http"
+
+	"nhooyr.io/websocket"
+)
+
+func WebSocketHandleFunc(w http.ResponseWriter, req *http.Request) {
+	// Accept 从客户端接受 WebSocket 握手，并将连接升级到 WebSocket
+	// 如果 Origin 域与主机不同，Accept 将拒绝握手，除非设置了 InsecureSkipVerify 选项（通过第三个参数 AcceptOptions 设置）
+	// 换句话说，默认情况下，它不允许跨源请求。如果发生错误，Accept 将始终写入适当的响应
+	conn, err := websocket.Accept(w, req, &websocket.AcceptOptions{InsecureSkipVerify: true})
+	if err != nil {
+		log.Println("websocket accept error:", err)
+		return
+	}
+
+	// 1. 新用户进来，构建该用户的实例
+	
+}
