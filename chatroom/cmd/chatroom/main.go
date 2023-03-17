b856e9ea538cb223f387e99884008004efc275cc
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-17 11:41:18 +0800

    Update main.go

--- chatroom/cmd/chatroom/main.go
+++ chatroom/cmd/chatroom/main.go
@@ -26,9 +26,12 @@ func init() {
 }
 
 func main() {
+	// 在服务器上打印开始界面，一表示项目启动
 	fmt.Printf(banner, addr)
 
+	// 初始化路由
 	server.RegisterHandle()
 
+	// 监听并服务指定端口，出错就写下日志并 os.exit(1)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
