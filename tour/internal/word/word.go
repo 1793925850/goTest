736b08411912ba8b974f277af2ec83f00461fa86
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-17 16:33:31 +0800

    update

--- tour/internal/word/word.go
+++ tour/internal/word/word.go
@@ -1,1 +1,51 @@
 package word
+
+import (
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+// ToUpper 把单词全部转为大写
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+// ToLower 把单词全部转为小写
+func ToLower(s string) string {
+	return strings.ToLower(s)
+}
+
+// UnderscoreToUpperCamelCase 下划线单词转大写驼峰单词
+func UnderscoreToUpperCamelCase(s string) string {
+	s = strings.Replace(s, "_", " ", -1)
+	s = cases.Title(language.English).String(s)
+
+	return strings.Replace(s, " ", "", -1)
+}
+
+// UnderscoreToLowerCamelCase 下划线单词转小写驼峰单词
+func UnderscoreToLowerCamelCase(s string) string {
+	s = UnderscoreToUpperCamelCase(s)
+
+	return string(unicode.ToLower(rune(s[0]))) + s[1:]
+}
+
+// CamelCaseToUnderscore 驼峰单词转下划线单词
+func CamelCaseToUnderscore(s string) string {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			output = append(output, unicode.ToLower(r))
+			continue
+		}
+		if unicode.IsUpper(r) {
+			output = append(output, '_')
+		}
+		output = append(output, unicode.ToLower(r))
+	}
+
+	return string(output)
+}
