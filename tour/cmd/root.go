9f68b1e0350ae70f23d1fd7b465c8927a248220f
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-18 11:44:43 +0800

    Update root.go

--- tour/cmd/root.go
+++ tour/cmd/root.go
@@ -1,1 +1,18 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "",
+	Long:  "",
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	// 每一个子命令都存在一个命令树中
+	rootCmd.AddCommand(wordCmd)
+}
