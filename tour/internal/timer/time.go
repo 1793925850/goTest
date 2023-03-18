47c6730f0365891e2d40a8698166cc049160aabb
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-18 21:31:53 +0800

    Update time.go

--- tour/internal/timer/time.go
+++ tour/internal/timer/time.go
@@ -2,6 +2,21 @@ package timer
 
 import "time"
 
+// GetNowTime 获得指定时区的当前时间
 func GetNowTime() time.Time {
-	return time.Now()
+	// LoadLocation 根据名称获取特定时区的 Location 实例
+	location, _ := time.LoadLocation("Asia/Shanghai")
+
+	return time.Now().In(location)
+}
+
+// GetCalculateTime 根据指定持续时间来推算时间
+func GetCalculateTime(currentTimer time.Time, d string) (time.Time, error) {
+	// ParseDuration 解析持续时间(可以解析后缀来确定时间的单位)
+	duration, err := time.ParseDuration(d)
+	if err != nil {
+		return time.Time{}, err // 解析出错，则返回一个空的 Time 的实例和错误
+	}
+
+	return currentTimer.Add(duration), nil
 }
