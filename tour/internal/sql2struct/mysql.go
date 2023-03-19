147c5d237e754fad191df3321bf1e989b07020df
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-19 21:22:03 +0800

    Update mysql.go

--- tour/internal/sql2struct/mysql.go
+++ tour/internal/sql2struct/mysql.go
@@ -2,6 +2,7 @@ package sql2struct
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -85,4 +86,34 @@ func (m *DBModel) Connect() error {
 	}
 
 	return nil
+}
+
+func (m *DBModel) GetColumns(dbName, tableName string) ([]*TableColumn, error) {
+	query := "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY, " +
+		"IS_NULLABLE, COLUMN_TYPE, COLUMN_COMMENT " +
+		"FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? "
+	rows, err := m.DBEngine.Query(query, dbName, tableName)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, errors.New("没有数据")
+	}
+
+	defer rows.Close()
+
+	var columns []*TableColumn
+
+	for rows.Next() {
+		var column TableColumn
+		err := rows.Scan(&column.ColumnName, &column.DataType, &column.ColumnKey, &column.IsNullable,
+			&column.ColumnType, &column.ColumnComment)
+		if err != nil {
+			return nil, err
+		}
+
+		columns = append(columns, &column)
+	}
+
+	return columns, nil
 }
