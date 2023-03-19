8f570e5cee5d0b1e8e3d68e54fa73e2396bbe2fa
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-19 20:41:16 +0800

    1

--- tour/internal/sql2struct/mysql.go
+++ tour/internal/sql2struct/mysql.go
@@ -1,1 +1,87 @@
 package sql2struct
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// DBModel 是整个数据库连接的核心对象
+type DBModel struct {
+	DBEngine *sql.DB
+	DBInfo   *DBInfo
+}
+
+// DBInfo 用于存储连接 MySQL 的一些基本信息
+type DBInfo struct {
+	DBType   string
+	Host     string
+	UserName string
+	PassWord string
+	Charset  string
+}
+
+// TableColumn 用于存储 COLUMNS 表中需要的一些字段
+type TableColumn struct {
+	ColumnName    string
+	DataType      string
+	IsNullable    string
+	ColumnKey     string
+	ColumnType    string
+	ColumnComment string
+}
+
+// DBTypeToStructType 数据库类型到 GO 结构体类型的映射
+var DBTypeToStructType = map[string]string{
+	"int":        "int32",
+	"tinyint":    "int8",
+	"smallint":   "int",
+	"mediumint":  "int64",
+	"bigint":     "int64",
+	"bit":        "int",
+	"bool":       "bool",
+	"enum":       "string",
+	"set":        "string",
+	"varchar":    "string",
+	"char":       "string",
+	"tinytext":   "string",
+	"mediumtext": "string",
+	"text":       "string",
+	"longtext":   "string",
+	"blob":       "string",
+	"tinyblob":   "string",
+	"mediumblob": "string",
+	"longblob":   "string",
+	"date":       "time.Time",
+	"datetime":   "time.Time",
+	"timestamp":  "time.Time",
+	"time":       "time.Time",
+	"float":      "float64",
+	"double":     "float64",
+}
+
+func NewDBModel(info *DBInfo) *DBModel {
+	return &DBModel{
+		DBInfo: info,
+	}
+}
+
+func (m *DBModel) Connect() error {
+	var err error
+	s := "%s:%s@tcp(%s)/information_schema?" + "charset=%s&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(
+		s,
+		m.DBInfo.UserName,
+		m.DBInfo.PassWord,
+		m.DBInfo.Host,
+		m.DBInfo.Charset,
+	)
+
+	m.DBEngine, err = sql.Open(m.DBInfo.DBType, dsn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
