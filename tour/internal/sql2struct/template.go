6b715c6b9aa393117db909f4de1b80bb51bd33b2
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-20 15:54:39 +0800

    Update template.go

--- tour/internal/sql2struct/template.go
+++ tour/internal/sql2struct/template.go
@@ -1,5 +1,12 @@
 package sql2struct
 
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"tour/internal/word"
+)
+
 const structTpl = `type {{.TableName | ToCamelCase}} struct {
 {{range .Columns}}	{{ $length := len .Comment}}	{{ if gt $length 0 }}//	{{.Comment}}	{{else}}//	{{.Name}} {{ end }}
 	{{ $typeLen := len .Type}} {{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}	{{.Type}}	{{.Tag}}{{ else }}{{.Name}}{{ end }}
@@ -23,4 +30,47 @@ type StructColumn struct {
 type StructTemplateDB struct {
 	TableName string
 	Columns   []*StructColumn
+}
+
+// NewStructTemplate 新建结构体模板
+func NewStructTemplate() *StructTemplate {
+	return &StructTemplate{
+		structTpl: structTpl,
+	}
+}
+
+// AssemblyColumns 将数据库中每个列对应的列名、数据类型、标签、注释更换成适用于 Go 的形式
+func (t *StructTemplate) AssemblyColumns(tbColumns []*TableColumn) []*StructColumn {
+	tplColumns := make([]*StructColumn, 0, len(tbColumns))
+
+	for _, column := range tbColumns {
+		tag := fmt.Sprintf("`"+"json:"+"\"%s\""+"`", column.ColumnName)
+
+		tplColumns = append(tplColumns, &StructColumn{
+			Name:    column.ColumnName,
+			Type:    DBTypeToStructType[column.DataType],
+			Tag:     tag,
+			Comment: column.ColumnComment,
+		})
+	}
+
+	return tplColumns
+}
+
+func (t *StructTemplate) Generate(tableName string, tplColumns []*StructColumn) error {
+	tpl := template.Must(template.New("sql2struct").Funcs(template.FuncMap{
+		"ToCamelCase": word.UnderscoreToUpperCamelCase,
+	}).Parse(t.structTpl))
+
+	tplDB := StructTemplateDB{
+		TableName: tableName,
+		Columns:   tplColumns,
+	}
+
+	err := tpl.Execute(os.Stdout, tplDB)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
