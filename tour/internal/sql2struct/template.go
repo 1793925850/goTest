56d34f62719a69f2cdd9c37bc778b4f48d089593
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-22 18:39:20 +0800

    Update template.go

--- tour/internal/sql2struct/template.go
+++ tour/internal/sql2struct/template.go
@@ -12,7 +12,7 @@ import (
 const structTpl = `type {{.TableName | ToCamelCase}} struct {
 {{range .Columns}}	{{ $length := len .Comment}}	{{ if gt $length 0 }}// {{.Comment}}	{{else}}// {{.Name}} {{ end }}
 	{{ $typeLen := len .Type}} {{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}	{{.Type}}	{{.Tag}}{{ else }}{{.Name}}{{ end }}
-{{end}}
+{{end}}}
 
 func (model {{.TableName | ToCamelCase}}) TableName() string {
 	return "{{.TableName}}"
