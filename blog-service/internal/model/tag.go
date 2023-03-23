a98666cb618b1eaf462053514ae61dd05142264d
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-23 15:30:39 +0800

    添加model

---
+++ blog-service/internal/model/tag.go
@@ -0,0 +1,12 @@
+package model
+
+type Tag struct {
+	*Model        // 复用公共 Model
+	Name   string `json:"name"`
+	State  uint8  `json:"state"`
+}
+
+// 这里我在 Tag 前加了个 *
+func (t *Tag) TableName() string {
+	return "blog_tag"
+}
