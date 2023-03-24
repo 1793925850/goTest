e3ec3847b58aa2f8f3cd6c22efa1fac24ed8a8f7
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-24 16:01:00 +0800

    连接数据库

--- blog-service/internal/model/model.go
+++ blog-service/internal/model/model.go
@@ -1,5 +1,14 @@
 package model
 
+import (
+	"blog-service/global"
+	"blog-service/pkg/setting"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 // 公共 model
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
@@ -9,4 +18,26 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	// 进入到指定的 mysql 的数据库中
+	// db 是数据库连接
+	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf(
+		s,
+		databaseSetting.Username,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime))
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+
 }
