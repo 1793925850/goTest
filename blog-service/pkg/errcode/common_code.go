f9aa6a96a70bb1671407ced8072e0aef2fd3cc64
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-24 09:08:12 +0800

    新建文件

---
+++ blog-service/pkg/errcode/common_code.go
@@ -0,0 +1,15 @@
+package errcode
+
+// 公共错误码
+
+var (
+	Success                   = NewError(0, "成功")
+	ServerError               = NewError(100000000, "服务内部错误")
+	InvalidParams             = NewError(100000001, "入参错误(无效参数)")
+	NotFound                  = NewError(100000002, "没有找到")
+	UnauthorizedAuthNotExist  = NewError(100000003, "鉴权失败，找不到对应的AppKey和AppSecret")
+	UnauthorizedTokenError    = NewError(100000004, "鉴权失败，Token错误")
+	UnauthorizedTokenTimeout  = NewError(100000005, "鉴权失败，Token超时")
+	UnauthorizedTokenGenerate = NewError(100000006, "鉴权失败，Token生成失败")
+	TooManyRequests           = NewError(100000007, "请求过多")
+)
