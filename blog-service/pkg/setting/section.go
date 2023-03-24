adc9d56ec73b741d024dd1061d30a0bab2c94520
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-24 11:32:18 +0800

    Create section.go

---
+++ blog-service/pkg/setting/section.go
@@ -0,0 +1,44 @@
+package setting
+
+// 用于声明配置属性的结构体，并编写读取区段配置的配置方法
+
+import "time"
+
+type ServerSettingS struct {
+	RunMode       string
+	HttpPort      string
+	ReadTimeout   time.Duration
+	WriterTimeout time.Duration
+}
+
+type AppSettingS struct {
+	DefaultPageSize int
+	MaxPageSize     int
+	LogSavePath     string
+	LogFileName     string
+	LogFileExt      string
+}
+
+type DatabaseSettingS struct {
+	DBType       string
+	Username     string
+	Password     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+// ReadSection 读取区段配置
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	// UnmarshalKey 获取单个键并将其解组为 Struct
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
