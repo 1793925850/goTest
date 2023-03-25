85ad97d7773e201d7e62dbb0a8617f8cf1b6500e
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-25 16:23:10 +0800

    Create pagination.go

---
+++ blog-service/pkg/app/pagination.go
@@ -0,0 +1,18 @@
+package app
+
+// 分页处理
+
+import (
+	"blog-service/pkg/convert"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetPage(c *gin.Context) int {
+	page := convert.StrTo(c.Query("page")).MustInt()
+	if page <= 0 {
+		return 1
+	}
+
+	return page
+}
