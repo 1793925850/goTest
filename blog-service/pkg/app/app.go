3ad68bc23004be3f1b69532b920161f63ca90f2f
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-25 17:19:50 +0800

    1

--- blog-service/pkg/app/app.go
+++ blog-service/pkg/app/app.go
@@ -4,7 +4,13 @@ package app
 响应处理
 */
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"blog-service/pkg/errcode"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Response struct {
 	Ctx *gin.Context
@@ -18,3 +24,32 @@ type Pager struct {
 	// 总行数
 	TotalRows int `json:"total_rows"`
 }
+
+// NewResponse 初始化响应
+func NewResponse(ctx *gin.Context) *Response {
+	return &Response{
+		Ctx: ctx,
+	}
+}
+
+// ToResponse 初始化响应
+func (r *Response) ToResponse(data interface{}) {
+	if data == nil {
+		data = gin.H{}
+	}
+	r.Ctx.JSON(http.StatusOK, data)
+}
+
+// ToResponseList 初始化响应列表
+func (r *Response) ToResponseList(list interface{}, totalRows int) {
+	r.Ctx.JSON(http.StatusOK, gin.H{
+		"list": list,
+		"pager": Pager{
+			Page:      GetPage(r.Ctx),
+			PageSize:  GetPageSize(r.Ctx),
+			TotalRows: totalRows,
+		},
+	})
+}
+
+func (r *Response) ToErrorResponse(err *errcode.Error)
