b6450b31756579a10936d4ded5c096d6bf1cab54
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-25 14:17:11 +0800

    Update setting.go

--- blog-service/global/setting.go
+++ blog-service/global/setting.go
@@ -2,11 +2,15 @@ package global
 
 // 将配置信息和应用程序关联起来
 
-import "blog-service/pkg/setting"
+import (
+	"blog-service/pkg/logger"
+	"blog-service/pkg/setting"
+)
 
 // 配置的全局变量
 var (
 	ServerSetting   *setting.ServerSettingS
 	AppSetting      *setting.AppSettingS
 	DatabaseSetting *setting.DatabaseSettingS
+	Logger          *logger.Logger
 )
