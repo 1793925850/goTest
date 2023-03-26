a7a9edfb67d0b67958f01384e8bf3a89cba33b2e
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-26 13:51:20 +0800

    1

--- blog-service/internal/service/article.go
+++ blog-service/internal/service/article.go
@@ -1,34 +1,32 @@
 package service
 
 /**
-标签接口
+文章接口
 */
 
 // 针对入参校验增加绑定和验证结构体
 
-type CountTagRequest struct {
+type ArticleRequest struct {
+}
+
+type ArticleListRequest struct {
 	Name  string `form:"name" binding:"max=100"`
 	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
-type TagListRequest struct {
-	Name  string `form:"name" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type CreateTagRequest struct {
+type CreateArticleRequest struct {
 	Name      string `form:"name" binding:"required,min=2,max=100"`
 	CreatedBy string `form:"created_by" binding:"required,min=2,max=100"`
 	State     uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
-type UpdateTagRequest struct {
+type UpdateArticleRequest struct {
 	ID         uint32 `form:"id" binding:"required,gte=1"`
 	Name       string `form:"name" binding:"max=100"`
 	State      uint8  `form:"state" binding:"oneof=0 1"`
 	ModifiedBy string `form:"modified_by" binding:"required,min=2,max=100"`
 }
 
-type DeleteTagRequest struct {
+type DeleteArticleRequest struct {
 	ID uint32 `form:"id" binding:"required,gte=1"`
 }
