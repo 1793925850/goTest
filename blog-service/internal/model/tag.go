f70d5aa3bda98c4b97cf3a1ff0e43642c044fce5
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-30 11:24:51 +0800

    Update tag.go

--- blog-service/internal/model/tag.go
+++ blog-service/internal/model/tag.go
@@ -13,7 +13,7 @@ import (
 // 标签 model
 type Tag struct {
 	*Model        // 复用公共 Model
-	Name   string `json:"name"` // 标签名称
+	Name   string `json:"name"`  // 标签名称
 	State  uint8  `json:"state"` // 状态
 }
 
