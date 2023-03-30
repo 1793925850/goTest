d16d23aff57460fbaf6494b7eb0b1494fcec9270
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-03-30 11:17:41 +0800

    Update tag.go

--- blog-service/internal/model/tag.go
+++ blog-service/internal/model/tag.go
@@ -1,6 +1,14 @@
 package model
 
-import "blog-service/pkg/app"
+import (
+	"blog-service/pkg/app"
+
+	"github.com/jinzhu/gorm"
+)
+
+/**
+针对 blog_tag 表进行处理，对标签模块的模型操作进行封装
+*/
 
 // 标签 model
 type Tag struct {
@@ -14,6 +22,82 @@ type TagSwagger struct {
 	Pager *app.Pager
 }
 
+// TableName 默认解析该结构体的名字为表名
 func (t Tag) TableName() string {
 	return "blog_tag"
+}
+
+// Count 统计行为，用于统计某个表的指定的记录的数量。输入参数指定了某条数据库连接
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+
+	if t.Name != "" {
+		// Where 设置筛选条件，接受 map、struct、或 string 作为条件
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+
+	// 筛选 is_del 的值为0的指定标签(tag)，并计数
+	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// List 将某个表中符合条件的记录全部列举出来
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+// ListByIDs 通过 tag 的 id 来列举符合条件的记录
+func (t Tag) ListByIDs(db *gorm.DB, ids []uint32) ([]*Tag, error) {
+	var tags []*Tag
+
+	db = db.Where("state = ? AND is_del = ?", t.State, 0)
+	err := db.Where("id IN (?)", ids).Find(&tags).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+// Get 查找某个标签的第一条记录
+func (t Tag) Get(db *gorm.DB) (Tag, error) {
+	var tag Tag
+	err := db.Where("id = ? AND is_del = ? AND state = ?", t.ID, 0, t.State).First(&tag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return tag, err
+	}
+
+	return tag, nil
+}
+
+// Create 创建一个新标签
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB, values interface{}) error {
+	// Model 会调用 TableName 方法来获取用来操作的表名
+	return db.Model(&t).Where("id = ? AND is_del = ?", t.ID, 0).Update(values).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+
 }
