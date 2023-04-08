54aaea72b33a2a7420c182052f58cc0b7e2f92be
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-04-08 20:58:18 +0800

    Update article.go

--- blog-service/internal/service/article.go
+++ blog-service/internal/service/article.go
@@ -6,16 +6,19 @@ package service
 
 // 针对入参校验增加绑定和验证结构体
 
+// ArticleRequest 文章请求
 type ArticleRequest struct {
 	ID    uint32 `form:"tag_id" binding:"required,gte=1"`
 	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
+// ArticleListRequest 文章列表请求
 type ArticleListRequest struct {
 	TagID uint32 `form:"tag_id" binding:"gte=1"`
 	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
+// CreateArticleRequest 创建文章请求
 type CreateArticleRequest struct {
 	TagID         uint32 `form:"tag_id" binding:"gte=1"`
 	Title         string `form:"title" binding:"required,min=2,max=100"`
@@ -26,6 +29,7 @@ type CreateArticleRequest struct {
 	State         uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
+// UpdateArticleRequest 更新文章请求
 type UpdateArticleRequest struct {
 	ID            uint32 `form:"id" binding:"required,gte=1"`
 	TagID         uint32 `form:"tag_id" binding:"required,gte=1"`
@@ -37,6 +41,7 @@ type UpdateArticleRequest struct {
 	State         uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
+// DeleteArticleRequest 删除文章请求
 type DeleteArticleRequest struct {
 	ID uint32 `form:"id" binding:"required,gte=1"`
 }
