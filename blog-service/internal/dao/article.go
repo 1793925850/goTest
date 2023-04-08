7a27f982e45ff7a5edc9e6f565c15be4f7603e69
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-04-08 20:52:09 +0800

    Update article.go

--- blog-service/internal/dao/article.go
+++ blog-service/internal/dao/article.go
@@ -1,6 +1,9 @@
 package dao
 
-import "blog-service/internal/model"
+import (
+	"blog-service/internal/model"
+	"blog-service/pkg/app"
+)
 
 /**
 用于处理文章模块的 dao 操作
@@ -33,6 +36,7 @@ func (d *Dao) CreateArticle(param *Article) (*model.Article, error) {
 	return article.Create(d.engine)
 }
 
+// UpdateArticle 更新文章
 func (d *Dao) UpdateArticle(param *Article) error {
 	article := model.Article{Model: &model.Model{ID: param.ID}}
 	values := map[string]interface{}{
@@ -54,4 +58,35 @@ func (d *Dao) UpdateArticle(param *Article) error {
 	}
 
 	return article.Update(d.engine, values)
+}
+
+// GetArticle 获取文章
+func (d *Dao) GetArticle(id uint32, state uint8) (model.Article, error) {
+	article := model.Article{
+		Model: &model.Model{ID: id},
+		State: state,
+	}
+
+	return article.Get(d.engine)
+}
+
+// DeleteArticle 删除文章
+func (d *Dao) DeleteArticle(id uint32) error {
+	article := model.Article{Model: &model.Model{ID: id}}
+
+	return article.Delete(d.engine)
+}
+
+// CountArticleListByTagID 通过标签ID，统计罗列出的文章数量
+func (d *Dao) CountArticleListByTagID(id uint32, state uint8) (int, error) {
+	article := model.Article{State: state}
+
+	return article.CountByTagID(d.engine, id)
+}
+
+// GetArticleListByTagID 通过标签ID，获取文章列表
+func (d *Dao) GetArticleListByTagID(id uint32, state uint8, page, pageSize int) ([]*model.ArticleRow, error) {
+	article := model.Article{State: state}
+
+	return article.ListByTagID(d.engine, id, app.GetPageOffset(page, pageSize), pageSize)
 }
