e4348ec243685e99b4489fdbba033d49ea95d600
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-04-08 11:14:27 +0800

    Update article.go

--- blog-service/internal/model/article.go
+++ blog-service/internal/model/article.go
@@ -5,6 +5,10 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+/**
+model 层的封装
+*/
+
 // 文章 model
 type Article struct {
 	*Model
@@ -34,26 +38,40 @@ func (a Article) TableName() string {
 	return "blog_article"
 }
 
+// Create 创建一篇文章
 func (a Article) Create(db *gorm.DB) (*Article, error) {
+	if err := db.Create(&a).Error; err != nil {
+		return nil, err
+	}
 
+	return &a, nil
 }
 
+// Update 更新一篇文章
 func (a Article) Update(db *gorm.DB, values interface{}) error {
+	if err := db.Model(&a).Where("id = ? AND is_del = ?", a.ID, 0).Update(values).Error; err != nil {
+		return err
+	}
 
+	return nil
 }
 
+// Get 获取一篇文章
 func (a Article) Get(db *gorm.DB) (Article, error) {
 
 }
 
+// Delete 删除一篇文章
 func (a Article) Delete(db *gorm.DB) error {
 
 }
 
+// ListByTagID 通过标签ID来获得指定一系列文章
 func (a Article) ListByTagID(db *gorm.DB, tagID uint32, pageOffset, pageSize int) ([]*ArticleRow, error) {
 
 }
 
+// CountByTagID 统计有标签ID的文章的数量
 func (a Article) CountByTagID(db *gorm.DB, tagID uint32) (int, error) {
-	
+
 }
