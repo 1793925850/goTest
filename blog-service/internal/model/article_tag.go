9e922ecac383b6de18f9b405c8fd253c5d20b223
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-04-14 19:06:41 +0800

    Update article_tag.go

--- blog-service/internal/model/article_tag.go
+++ blog-service/internal/model/article_tag.go
@@ -35,22 +35,49 @@ func (a ArticleTag) ListByTID(db *gorm.DB) ([]*ArticleTag, error) {
 	return articleTags, nil
 }
 
+// ListByAIDs 通过文章ID罗列文章
 func (a ArticleTag) ListByAIDs(db *gorm.DB, articleIDs []uint32) ([]*ArticleTag, error) {
+	var articleTags []*ArticleTag
+	err := db.Where("article_id IN (?) AND is_del = ?", articleIDs, 0).Find(&articleTags).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
 
+	return articleTags, nil
 }
 
+// Create 创建文章
 func (a ArticleTag) Create(db *gorm.DB) error {
+	if err := db.Create(&a).Error; err != nil {
+		return err
+	}
 
+	return nil
 }
 
+// UpdateOne 更新文章
 func (a ArticleTag) UpdateOne(db *gorm.DB, values interface{}) error {
+	if err := db.Model(&a).Where("article_id = ? AND is_del = ?", a.ArticleID, 0).Limit(1).Update(values).Error; err != nil {
+		return err
+	}
 
+	return nil
 }
 
+// Delete 删除文章
 func (a ArticleTag) Delete(db *gorm.DB) error {
+	if err := db.Where("id = ? AND is_del = ?", a.Model.ID, 0).Delete(&a).Error; err != nil {
+		return err
+	}
 
+	return nil
 }
 
+// DeleteOne 删除文章
 func (a ArticleTag) DeleteOne(db *gorm.DB) error {
+	if err := db.Where("article_id = ? AND is_del = ?", a.ArticleID, 0).Delete(&a).Limit(1).Error; err != nil {
+		return err
+	}
 
+	return nil
 }
