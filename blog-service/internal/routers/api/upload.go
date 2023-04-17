61f1bdda0733003b01cf9b24cc77d53543cf1e4d
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-04-17 20:13:27 +0800

    Update upload.go

--- blog-service/internal/routers/api/upload.go
+++ blog-service/internal/routers/api/upload.go
@@ -1,6 +1,14 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog-service/global"
+	"blog-service/internal/service"
+	"blog-service/pkg/app"
+	"blog-service/pkg/convert"
+	"blog-service/pkg/errcode"
+	"blog-service/pkg/upload"
+	"github.com/gin-gonic/gin"
+)
 
 /**
 上传文件的路由方法
@@ -9,10 +17,35 @@ import "github.com/gin-gonic/gin"
 // 由于 golang 没有静态方法，因此只能用类方法当静态方法用
 type Upload struct{}
 
+// NewUpload 新建一个上传文件实例
 func NewUpload() Upload {
 	return Upload{}
 }
 
+// UploadFile 上传文件
 func (u Upload) UploadFile(c *gin.Context) {
+	response := app.NewResponse(c)
+	file, fileHeader, err := c.Request.FormFile("file")
+	if err != nil {
+		response.ToErrorResponse(errcode.InvalidParams.WithDetails(err.Error()))
+		return
+	}
 
+	fileType := convert.StrTo(c.PostForm("type")).MustInt()
+	if fileHeader == nil || fileType <= 0 {
+		response.ToErrorResponse(errcode.InvalidParams)
+		return
+	}
+
+	svc := service.New(c.Request.Context())
+	fileInfo, err := svc.UploadFile(upload.FileType(fileType), file, fileHeader)
+	if err != nil {
+		global.Logger.Errorf(c, "svc.UploadFile err: %v", err)
+		response.ToErrorResponse(errcode.ErrorUploadFileFail.WithDetails(err.Error()))
+		return
+	}
+
+	response.ToResponse(gin.H{
+		"file_access_url": fileInfo.AccessUrl,
+	})
 }
