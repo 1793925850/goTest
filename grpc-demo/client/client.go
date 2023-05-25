68dd9ba1b89a6ecbc9f27a2b6f5771ea9a1202bf
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-05-25 11:50:54 +0800

    添加SayList方法RPC

--- grpc-demo/client/client.go
+++ grpc-demo/client/client.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"google.golang.org/grpc"
 	pb "grpc-demo/proto"
+	"io"
 	"log"
 )
 
@@ -22,11 +23,28 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewGreeterClient(conn) // 创建 服务Greeter 的客户端对象
-	_ = SayHello(client)                // 发送 RPC 请求，等待同步响应，得到回调后返回响应结果
+	_ = SayList(client)                 // 发送 RPC 请求，等待同步响应，得到回调后返回响应结果
 }
 
 func SayHello(client pb.GreeterClient) error {
 	resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{Name: "eddycjy"})
 	log.Printf("client.SayHello resp: %s", resp.Message)
+	return nil
+}
+
+func SayList(client pb.GreeterClient) error {
+	stream, _ := client.SayList(context.Background(), &pb.HelloRequest{Name: "JinXiao"})
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("resp: %v", resp)
+	}
+
 	return nil
 }
--- grpc-demo/server/server.go
+++ grpc-demo/server/server.go
@@ -21,6 +21,14 @@ func (s *GreeterServer) SayHello(ctx context.Context, r *pb.HelloRequest) (*pb.H
 	return &pb.HelloReply{Message: "hello.world"}, nil
 }
 
+func (s *GreeterServer) SayList(r *pb.HelloRequest, stream pb.Greeter_SayListServer) error {
+	for n := 0; n <= 6; n++ {
+		_ = stream.Send(&pb.HelloReply{Message: "hello.list"})
+	}
+
+	return nil
+}
+
 func main() {
 	server := grpc.NewServer()                         // 创建服务器实例
 	pb.RegisterGreeterServer(server, &GreeterServer{}) // 在该服务器上注册 RPC服务（即 Greeter）
