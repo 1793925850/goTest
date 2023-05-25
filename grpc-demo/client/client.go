9a8c5e01b6436c8ce85808967892ea7be3f1a12b
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-05-25 11:30:53 +0800

    一元 RPC

---
+++ grpc-demo/client/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"google.golang.org/grpc"
+	pb "grpc-demo/proto"
+	"log"
+)
+
+type GreeterServer struct{}
+
+var port string
+
+func init() {
+	flag.StringVar(&port, "p", "8000", "启动端口号")
+	flag.Parse()
+}
+
+func main() {
+	conn, _ := grpc.Dial(":"+port, grpc.WithInsecure()) // 创建与给定目标（服务端）的连接句柄
+	defer conn.Close()
+
+	client := pb.NewGreeterClient(conn) // 创建 服务Greeter 的客户端对象
+	_ = SayHello(client)                // 发送 RPC 请求，等待同步响应，得到回调后返回响应结果
+}
+
+func SayHello(client pb.GreeterClient) error {
+	resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{Name: "eddycjy"})
+	log.Printf("client.SayHello resp: %s", resp.Message)
+	return nil
+}
---
+++ grpc-demo/server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"google.golang.org/grpc"
+	pb "grpc-demo/proto"
+	"net"
+)
+
+type GreeterServer struct{}
+
+var port string
+
+func init() {
+	flag.StringVar(&port, "p", "8000", "启动端口号")
+	flag.Parse()
+}
+
+func (s *GreeterServer) SayHello(ctx context.Context, r *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "hello.world"}, nil
+}
+
+func main() {
+	server := grpc.NewServer()                         // 创建服务器实例
+	pb.RegisterGreeterServer(server, &GreeterServer{}) // 在该服务器上注册 RPC服务（即 Greeter）
+	lis, _ := net.Listen("tcp", ":"+port)              // 创建监听器，服务器监听端口
+	server.Serve(lis)                                  // 监听到请求便返回指定服务结果
+}
